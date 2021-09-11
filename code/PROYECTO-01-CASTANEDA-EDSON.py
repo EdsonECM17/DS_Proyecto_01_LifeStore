ca62bcf75f18ee2f46a434556723b5f3b52f443a
Edson Castañeda <ecastaneda@diram.com>
2021-09-11 14:20:33 -0500

    feature: analisis de ventas por producto

--- code/PROYECTO-01-CASTANEDA-EDSON.py
+++ code/PROYECTO-01-CASTANEDA-EDSON.py
@@ -4,7 +4,7 @@ from data.lifestore_file import lifestore_products, lifestore_sales, lifestore_s
 admin_users = {'ecastaneda': 'lifestore12345', 'cguillen': 'holamundo'}
 
 # Login de usuario
-acesso = False # If true, accede a la información de LifeStore
+acesso = True # If true, accede a la información de LifeStore
 additional_attempts = 2 # Intentos extra en caso de login fallido
 user = input("Ingrese su usuario: ")
 password = input("Ingrese su contraseña: ")
@@ -26,7 +26,54 @@ while acesso is False and additional_attempts > 0:
 
 # Acceso correcto
 if acesso:
-    print("Acesso a Datos")
+    # ANALISIS DE VENTAS Y BUSQUEDAS POR PRODUCTO
+
+    # Inicializar variables relacionadas al producto
+    sales_per_product = {}  #  Ventas de cada productio (id_product:total_sales_of_product)
+    searches_per_product = {}  # Busquedas de cada productio (id_product:total_searches_of_product)
+    reviews_per_product_sum = {}  # Suma de reseñas (id_product:reviews_sum)
+    reviews_per_product_avg = {} # Promedio de reseñas de cada producto (id_product:reviews_avg)
+    refunds_per_product = {}  # Conteo de devoluciones por producto (id_product:refunds_sum)
+    refund_per_product_pct = {} # Porcentaje de devoluciones de producto respecto a total vendido (id_product:refunds_avg)
+
+    # Iniciar analisis de cada producto
+    for product in lifestore_products:
+        sales_per_product[product[0]] = 0
+        searches_per_product[product[0]] = 0
+        refunds_per_product[product[0]] = 0
+        reviews_per_product_sum[product[0]] = 0
+
+        # Contar ventas del producto y sumar reseñas y devoluciones
+        for sale in lifestore_sales:
+            if sale[1] == product[0]:
+                sales_per_product[product[0]] +=1
+                reviews_per_product_sum[product[0]]+= sale[2]
+                refunds_per_product[product[0]] += sale[4]
+        # Contar busqueda de producto
+        for search in lifestore_searches:
+            if search[1] == product[0]:
+                searches_per_product[product[0]] +=1
+
+    # Resumen de analisis por producto
+    # Identificar id de 50 productos mas vendidos
+    most_sold_products = sorted(sales_per_product, key=sales_per_product.get, reverse=True)[:50]
+    # Identificar id de 100 productos mas buscados
+    most_searched_products = sorted(searches_per_product, key=searches_per_product.get, reverse=True)[:100]
+    # Identificar id de 50 productos menos vendidos
+    less_sold_products = sorted(sales_per_product, key=sales_per_product.get)[:50]
+    # Identificar id de 100 productos menos buscados
+    less_searched_products = sorted(searches_per_product, key=searches_per_product.get)[:100]
+
+    # Obtener promedio de reseñas de cada producto y porcentaje de devoluaciones
+    for id_product in reviews_per_product_sum.keys():
+        # Procesar datos solo en casos de que existan ventas del producto
+        if sales_per_product[id_product] > 0:
+            reviews_per_product_avg[id_product] = reviews_per_product_sum[id_product]/sales_per_product[id_product]
+            refund_per_product_pct[id_product] = round((refunds_per_product[id_product]/sales_per_product[id_product])*100, 2)
+        else:
+            reviews_per_product_avg[id_product] = 0
+            refund_per_product_pct[id_product] = 0
+
 
 # 3 intentos fallidos
 else:
