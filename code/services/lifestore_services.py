02e55ee288f0b9c75c66a98d1cf4605445d98b14
Edson Castañeda <ecastaneda@diram.com>
2021-09-18 12:18:25 -0500

    feature: add month services and documentation
    - Add methods to get month data
    - Add documentation of services
    - Fix typos
    - Replace date format (fix bug in filter)

--- code/services/lifestore_services.py
+++ code/services/lifestore_services.py
@@ -1,3 +1,5 @@
+import calendar
+
 from processing.lifestore_tables import lifestore_products, lifestore_sales, lifestore_searches
 from processing.filters_df import Filters
 
@@ -20,7 +22,7 @@ class Service(Filters):
             end_date (str or None, optional): Fecha de inicio de periodo de ventas considerado. Defaults to None.
             score_min (int or None, optional): Calificación minima de venta. Defaults to None.
             score_max (int or None, optional): Calificación maxima de venta. Defaults to None.
-            refund_status (bool or None, optional):  Ventas devueltas (True) o no devluetas (False). Defaults to None.
+            refund_status (bool or None, optional): Ventas devueltas (True) o no devluetas (False). Defaults to None.
 
         Returns:
             int: Numero de ventas que tuvo el caso solicitado.
@@ -45,7 +47,7 @@ class Service(Filters):
             end_date (str or None, optional): Fecha de inicio de periodo de ventas considerado. Defaults to None.
             score_min (int or None, optional): Calificación minima de venta. Defaults to None.
             score_max (int or None, optional): Calificación maxima de venta. Defaults to None.
-            refund_status (bool or None, optional):  Ventas devueltas (True) o no devluetas (False). Defaults to None.
+            refund_status (bool or None, optional): Ventas devueltas (True) o no devluetas (False). Defaults to None.
 
         Returns:
             int: Total de ingresos para caso solicitado.
@@ -85,19 +87,98 @@ class Service(Filters):
         return searches_number
 
     def year_sales(self, year:int, id_product: int or None = None, refund_status: bool or None = None) -> int:
+        """ Obtiene el número de ventas anuales. Cuenta con algunos filtros opcionales que permiten
+            considerar unicamente un producto o descartar las ventas que terminaron en devolución.
+
+        Args:
+            year (int): Año seleccionado.
+            id_product (int or None, optional): Id de producto. Defaults to None.
+            refund_status (bool or None, optional): Filtro de devoluciones. Defaults to None.
+
+        Returns:
+            int: Número de ventas anuales
+        """
         # Definir fecha de inicio del año y fecha de fin de año para filtros
-        year_start= f"1/1/{year}"
-        year_end = f"31/12/{year}"
+        year_start= f"{year}-01-01"
+        year_end = f"{year}-12-31"
         # Usar función get ventas para obtener datos de ventas anuales con los filtros
         sales_number = self.get_sales_number(start_date=year_start, end_date=year_end,
                                              id_product=id_product, refund_status=refund_status)
         return sales_number
     
     def year_income(self, year:int, id_product: int or None = None, refund_status: bool or None = None) -> int:
+        """ Obtiene los ingresos anuales. Cuenta con algunos filtros opcionales que permiten
+            considerar unicamente un producto o descartar las ventas que terminaron en devolución.
+
+        Args:
+            year (int): Año seleccionado.
+            id_product (int or None, optional): Id de producto. Defaults to None.
+            refund_status (bool or None, optional): Filtro de devoluciones. Defaults to None.
+
+        Returns:
+            int: Ingresos anuales.
+        """
         # Definir fecha de inicio del año y fecha de fin de año para filtros
-        year_start= f"1/1/{year}"
-        year_end = f"31/12/{year}"
+        year_start= f"{year}-01-01"
+        year_end = f"{year}-12-31"
         # Usar función get ventas para obtener datos de ingresos anuales con los filtros
         income = self.get_income(start_date=year_start, end_date=year_end,
                                  id_product=id_product, refund_status=refund_status)
         return income
+
+
+    def month_sales(self, year: int, id_product: int or None = None, refund_status: bool or None = None) -> dict:
+        """ Obtiene el número de ventas de cada mes. Cuenta con algunos filtros opcionales que permiten
+            considerar unicamente un producto o descartar las ventas que terminaron en devolución.
+            Las ventas de cada mes se organizan en un diccionario. 
+
+        Args:
+            year (int): Año seleccionado.
+            id_product (int or None, optional): Id de producto. Defaults to None.
+            refund_status (bool or None, optional): Filtro de devoluciones. Defaults to None.
+
+        Returns:
+            dict: Número de ventas de cada mes.
+        """
+        # Inicializa variables
+        sales_dict = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0} # month: sales_number_of_month
+        # Ciclo for para obtener el numero de ventas de cada mes
+        for month in range(1,13):
+            # Obtener cantidad de dias del mes para saber ultimo dia
+            month_days = calendar.monthrange(year, month)[1]
+            # Definir fecha de inicio del año y fecha de fin de año para filtros
+            month_start = f"{year}-{month:02d}-01"
+            month_end = f"{year}-{month:02d}-{month_days:02d}"
+            # Obtener ventas de ese mes
+            sales_dict[month] = self.get_sales_number(start_date=month_start, end_date=month_end,
+                                                      id_product=id_product, refund_status=refund_status)
+
+        return sales_dict
+        
+
+    def month_income(self, year: int, id_product: int or None = None, refund_status: bool or None = None) -> dict:
+        """ Obtiene los ingresos mensuales. Cuenta con algunos filtros opcionales que permiten
+            considerar unicamente un producto o descartar las ventas que terminaron en devolución.
+            Las ventas de cada mes se organizan en un diccionario.
+
+        Args:
+            year (int): Año seleccionado.
+            id_product (int or None, optional): Id de producto. Defaults to None.
+            refund_status (bool or None, optional): Filtro de devoluciones. Defaults to None.
+
+        Returns:
+            dict: Ingresos de cada mes.
+        """
+        # Inicializa variables
+        income_dict = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0} # month: income_of_month
+        # Ciclo for para obtener los ingresos de cada mes
+        for month in range(1,13):
+            # Obtener cantidad de dias del mes para saber ultimo dia
+            month_days = calendar.monthrange(year, month)[1]
+            # Definir fecha de inicio del año y fecha de fin de año para filtros
+            month_start = f"{year}-{month:02d}-01"
+            month_end = f"{year}-{month:02d}-{month_days:02d}"
+            # Obtener ventas de ese mes
+            income_dict[month] = self.get_income(start_date=month_start, end_date=month_end,
+                                                 id_product=id_product, refund_status=refund_status)
+        return income_dict
